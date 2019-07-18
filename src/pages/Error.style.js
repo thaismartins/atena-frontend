68a1d33f295c76b2fd30a11330348712e93981df
Thais Martins <contato@thaismartins.co>
2019-07-17 21:30:25 -0300

    Adding error page

---
+++ src/pages/Error.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+import Circle from "../assets/circulos.png";
+import Lines from "../assets/lines.png";
+import Imgbg from "../assets/img-bg.svg";
+
+const StyledScreenHowItWorks = styled.section`
+  .layout {
+    padding-top: 0;
+    background-color: #fdfdfd;
+    color: #666c71;
+  }
+`;
+
+export default StyledScreenHowItWorks;
